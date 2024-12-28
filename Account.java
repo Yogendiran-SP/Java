a06a1b7686724e1ea0932d7c688044ce13b62e87
Yogendiran-SP <jyogendiran@gmail.com>
2024-12-28 16:13:40 +0530

    Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,14 @@
-public class Account {
-    
+public class Account
+{
+    private double balance=500.00;
+    public double getBalance(int accountId)
+    {
+        return balance;
+    }
+    public static void main(String[] args)
+    {
+        Account accnt=new Account();
+        double value=accnt.getBalance(123456);
+        System.out.print("\tThe Balance is " + value);
+    }
 }
