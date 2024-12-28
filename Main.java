98dc1f499388f02330f38974de82f3af0b041b72
Yogendiran-SP <jyogendiran@gmail.com>
2024-12-29 00:39:36 +0530

    Late night update but printing name

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println(123);
-        System.out.println(10 + 20);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + "!");
+
     }
 }
--- StudentTester.java
+++ StudentTester.java
@@ -1,8 +1,15 @@
+
+
 public class StudentTester
 {
+    private int StuId;
+    public int getStudentId(int x){
+        return StuId;
+    }
     public static void main(String[] args)
     {
-        
+        StudentTester Student = new StudentTester();
+        int StudentId = Student.getStudentId(1001);
 
     }
 }
