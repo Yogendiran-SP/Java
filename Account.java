323447dd6227cb1f883070b20587bd3af7a2f6c5
Yogendiran-SP <jyogendiran@gmail.com>
2025-01-14 08:50:58 +0530

    updated

--- Account.java
+++ Account.java
@@ -6,7 +6,7 @@ class Account {
     }
   
     public static void main(String[] args) {
-      Account accnt = new Account();            // object creation
+      Account accnt = new Account();           // object creation
       double value = accnt.getBalance(123456);
       System.out.println("The balance is: " + value);
     }
