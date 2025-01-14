66dfa0266709e492aab2c2964c26dec12b0376b7
Yogendiran-SP <jyogendiran@gmail.com>
2025-01-14 13:09:08 +0530

    updated

--- Discount.java
+++ Discount.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class Discount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your bill ID: ");
         int billid = scanner.nextInt();
+        System.out.println("Enter your customer ID: ");
         int customerid = scanner.nextInt();
+        System.out.println("Enter your bill amount: ");
+        int billamt = scanner.nextInt();
+        System.out.println("Enter the discount: ");
         String discountval = scanner.next();
         double discountamt = Double.parseDouble(discountval.replace("%","")) / 100.0;
-        
+        double discountbillamt = billamt - (billamt*discountamt);
+        System.out.println("The discounted amount is "+discountbillamt);
+
         scanner.close();
     }
 }
