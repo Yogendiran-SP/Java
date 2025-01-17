a652e5d75e6f147e1cffae3a85620766e0c69535
Jidendiran-coder <joel.jade.networking@gmail.com>
2025-01-17 23:33:36 +0530

    updated

---
+++ Exercise Programs/EmployeeRecord.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EmployeeRecord {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the no. of employees salary to be given (upto a max of 100 ata a time): ");
+        float n = scanner.nextInt();
+        int lavg=0,gavg=0;
+        double avg=0;
+        double[] a= new double[100];
+        for(int i=0;i<n;i++) {
+            a[i]= scanner.nextDouble();
+        }
+        for(int i=0;i<n;i++) {
+            avg=avg+a[i];
+        }
+        avg=avg/n;
+        System.out.println("The average salary is "+avg);
+        for(int i=0;i<n;i++) {
+            if(a[i]<=avg) {
+                lavg = lavg+1;
+            }
+            else {
+                gavg = gavg+1;
+            }
+        }
+        System.out.println("The no. of employees salaries lesser than average is: "+lavg);
+        System.out.println("The no. of employees salaries greater than average is: "+gavg);
+        scanner.close();
+    }
+}
