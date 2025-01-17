c8ddefe12259014f40aa91f16a4ef20b053f3259
Yogendiran-SP <jyogendiran@gmail.com>
2025-01-17 09:42:37 +0530

    updated

---
+++ ReverseCalculatorFor.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ReverseCalculatorFor {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number to be reversed: ");
+        int a = s.nextInt();
+        String c = Integer.toString(a);
+        int b = 0, m = 1;
+        int i = (c.length());
+        if(i>1) {
+            for(int j=2;j<=i;j++) {
+                m=m*10;
+            }
+        }
+        i=i-1;
+        System.out.println("i value:"+i);
+        System.out.println("m value:"+m);
+        for(;a>0;) {
+            b=b+m*(a%10);
+            a=a/10;
+            m=m/10;
+            System.out.println("Iteration:"+b);
+        }
+        System.out.println("Last:"+b);
+        s.close();
+    }
+}
